e937168e82de0210ff977946e396f0daaa50c91e
Tycho Collins <tychocollins2020@fau.edu>
2024-11-25 16:49:50 -0500

    Initial Commit

---
+++ PawPal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PawPal
+//
+//  Created by Tycho Collins on 11/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PawPal/PawPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawPalApp.swift
+//  PawPal
+//
+//  Created by Tycho Collins on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PawPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
